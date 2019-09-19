15c0450eb9461ee68812443d4284389355e677fd
Dean Harrington <dharrington@armsbusinesssolutions.com>
2019-09-18 23:51:29 -0500

    Fixed json parsing of executor aware monitor

--- src/main/java/org/cohesive/envoy/event/monitor/ExecutorAwareMonitor.java
+++ src/main/java/org/cohesive/envoy/event/monitor/ExecutorAwareMonitor.java
@@ -6,9 +6,12 @@ import java.util.concurrent.ScheduledExecutorService;
 import org.cohesive.envoy.event.Monitor;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Component
 public abstract class ExecutorAwareMonitor extends Monitor {
 
+	@JsonIgnore
 	ScheduledExecutorService executorService  = Executors.newScheduledThreadPool(5);	
 	
 
